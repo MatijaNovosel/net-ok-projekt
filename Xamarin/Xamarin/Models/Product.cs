28e4d5ea0c37eea4d8376a48ece4b409831ced75
MatijaNovosel <mnovosel5@gmail.com>
2021-03-04 22:59:03 +0100

    Xamarin wip

---
+++ Xamarin/Xamarin/Models/Product.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace Xamarin.Models
+{
+    public class Tag
+    {
+        public int Id { get; set; }
+        public string Description { get; set; }
+    }
+}
---
+++ Xamarin/Xamarin/Models/Tag.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Xamarin.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public double Price { get; set; }
+        public DateTime MadeAt { get; set; }
+        public double Discount { get; set; }
+        public virtual List<Tag> Tags { get; set; }
+    }
+}
